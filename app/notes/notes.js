bb0c01b1c4f7ad9360e58281267d6cc90df0e9b0
Gavin Rulon <gavin.rulon@icloud.com>
2015-08-20 10:50:00 -0400

    Retrieve notes from Nevernote API and display in sidebar.

--- app/notes/notes.js
+++ app/notes/notes.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var nevernoteBasePath = 'http://nevernote-1150.herokuapp.com/api/v1/';
+var apiKey = '$2a$10$J4Ms85pRsVHTh3I.tconeuC5F6.15z/Rvt62hYf4E70WbBkKeHnb2';
+
 angular.module('notely.notes',['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
@@ -9,7 +12,9 @@ angular.module('notely.notes',['ngRoute'])
   });
 }])
 
-.controller('NotesController',function(){
-  
-
-});
+.controller('NotesController', ['$scope', '$http' ,function($scope, $http){
+  $http.get(nevernoteBasePath + 'notes?api_key=' + apiKey)
+    .success(function(notesData) {
+      $scope.notes = notesData;
+    });
+}]);
